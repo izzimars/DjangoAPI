44041e9a89eb282677dd8b7cb01f0e346049b2e9
Macaulay Israel <israelmacaulay8@gmail.com>
2023-08-24 08:27:03 -0400

    Initial commit

---
+++ polls/apiviews.py
@@ -0,0 +1,31 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .models import Poll, Choice
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    
+    def options(self, request, *args, **kwargs):
+        # You can customize the response headers here
+        response = Response()
+        response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return response
+
+
+
+class PollDetail(generics.RetrieveDestroyAPIView): 
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
